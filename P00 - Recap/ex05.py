54c2fafb198b4128d0a2b83cc998139376a85b0f
smmcoelho <77806811+smmcoelho@users.noreply.github.com>
2026-03-11 21:40:37 +0000

    fix

--- P00 - Recap/ex05.py
+++ P00 - Recap/ex05.py
@@ -2,11 +2,15 @@
 
 
 # if
-a = 0
+a = 7
 if a == 3:
     print("Numero 3")
 elif a == 4:
     print("Numero 4")
+elif a in [5, 6]:
+    print("Numero entre 5 e 6")
+elif a in range(7, 9):
+    print("Numero entre 7 e 8")
 else:
     print("Outro numero qualquer")
 
