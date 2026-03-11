5ab20eb0dd69a9fffe05275091ea0b18ff892c5d
smmcoelho <77806811+smmcoelho@users.noreply.github.com>
2026-03-11 14:09:37 +0000

    ex1

---
+++ P01/ex01.py
@@ -0,0 +1,17 @@
+# Tipos de Variáveis 
+
+# int
+num = 5 # numero inteiro
+print(f"O numero {num} 'e do tipo {type(num)}")
+
+# float
+numd = 5.5 # numero real 
+print(f"O numero {numd} 'e do tipo {type(numd)}")
+
+# str (string)
+text = "hello" # texto
+print(f"O numero {text} 'e do tipo {type(text)}")
+
+# bool (boolean)
+isValid = True # booleano (True or False)
+print(f"O numero {isValid} 'e do tipo {type(isValid)}")
