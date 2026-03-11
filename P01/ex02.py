098523086591fd5c89fd35714a602e415dd0f4e1
smmcoelho <77806811+smmcoelho@users.noreply.github.com>
2026-03-11 14:21:38 +0000

    Add example data structures in ex02.py

---
+++ P01/ex02.py
@@ -0,0 +1,19 @@
+# Estruturas de dados
+
+
+# list
+l = [1, 3, 5, 7, 9]
+print(f"A variable do tipo {type(l)} tem os valores: {l} ")
+
+# set
+s = [1, 3, 5, 7, 9]
+print(f"A variable do tipo {type(s)} tem os valores: {s} ")
+
+# tuple
+t = [1, 3, 5, 7, 9]
+print(f"A variable do tipo {type(t)} tem os valores: {t} ")
+
+# dic (dictionary)
+d = [1, 3, 5, 7, 9]
+print(f"A variable do tipo {type(d)} tem os valores: {d} ")
+
