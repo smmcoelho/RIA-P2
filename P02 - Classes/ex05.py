d1e471146b049874c3882db3c43e38acc79d0b2e
smmcoelho <77806811+smmcoelho@users.noreply.github.com>
2026-03-20 14:00:19 +0000

    Implement class diagram and methods for Coordenadas
    Add class diagram and method descriptions for Coordenadas

---
+++ P02 - Classes/ex05.py
@@ -0,0 +1,10 @@
+# Faz o diagrama de classes da classe Coordenadas e implementa em python
+# Propriedades:
+#   X
+#   Y
+# Metodos
+#   Imprimir coordenadas
+#   Somar duas Coordenadas
+#   Comparar Coordenadas
+#   Distancia entre duas coordenadas
+#   Imprimir um vector dada uma coordenadas em polares
