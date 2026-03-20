009f21d6862a1c74ad21546cbecd5c09744bd6a0
smmcoelho <77806811+smmcoelho@users.noreply.github.com>
2026-03-20 13:58:42 +0000

    Implement Carro class with properties and methods
    Add class diagram and implementation for Carro class.

---
+++ P02 - Classes/ex04.py
@@ -0,0 +1,12 @@
+# Faz o diagrama de classes da classe Carro e implementa em python
+# Propriedades:
+#   Marca
+#   Modelo
+#   Matricula
+#   Cor
+# Metodos:
+#   Obter modelo (marca + modelo)
+#   Obter matricula
+#   Alterar matricula
+#   Obter cor
+#   Alterar cor
