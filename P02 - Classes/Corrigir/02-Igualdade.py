3a5451dd150a0ff6461c45faacf227d75470473b
smmcoelho <77806811+smmcoelho@users.noreply.github.com>
2026-04-01 13:39:55 +0100

    Add equality comparison to Carro class
    Implemented equality comparison for Carro class.

---
+++ P02 - Classes/Corrigir/02-Igualdade.py
@@ -0,0 +1,12 @@
+# Corrigir codigo abaixo
+# Tip: implementar __str__ , __repr__ ou __format__
+
+class Carro:
+    def __init__(self, marca, modelo):
+        self.__marca = marca
+        self.__modelo = modelo
+
+if __name__ == '__main__':
+    c1 = Carro("Fiat", "Uno")
+    c2 = Carro("Fiat", "Uno")
+    print(c1 == c2)   #True
