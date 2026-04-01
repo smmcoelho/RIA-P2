4d1f4fd0f20ea62fcb889309dfe1cba8618b6c45
smmcoelho <77806811+smmcoelho@users.noreply.github.com>
2026-04-01 13:40:23 +0100

    Create Produto class with initialization and comments
    Implemented the Produto class with __init__ method and added placeholders for __add__, __str__, __repr__, or __format__ methods.

---
+++ P02 - Classes/Corrigir/03-Soma.py
@@ -0,0 +1,20 @@
+# Corrigir codigo abaixo
+# Tip: implementar __add__
+# Nao esquecer a  __str__ , __repr__ ou __format__
+
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.__nome = nome
+        self.__preco = preco
+
+
+if __name__ == '__main__':
+    p1 = Produto("Camisa", 50)
+    p2 = Produto("Camisola", 120)
+
+    print(p1)
+    print(p2)
+
+    total = p1 + p2
+    print("Total:", total)  # Total: 170
